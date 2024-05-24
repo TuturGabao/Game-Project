bfac73181912362a63aa6c250f7ce3768aa53aa1
TuturGabao <129973798+TuturGabao@users.noreply.github.com>
2024-05-24 16:27:27 +0100

    Update platform2.py
    Added a simple menu

--- platform2.py
+++ platform2.py
@@ -1,12 +1,13 @@
 import pygame
 import math
+import pygame_gui
 
 pygame.init()
 
 WIDTH, HEIGHT = 1200, 700
 
 STARTING_Y = 449
-STARTING_X = 800
+STARTING_X = 150
 
 PLAYER_HEIGHT = 25
 PLAYER_WIDTH = 15
@@ -26,6 +27,64 @@ ENEMY_SPEED = 3
 JUMPING_HEIGHT = 15
 
 FPS = 60
+FPS_UI = 1000
+
+BUTTON_HEIGHT, BUTTON_WIDTH = 40, 140
+
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+
+play_button_rect = pygame.Rect(WIDTH/2-BUTTON_WIDTH/2, 150, BUTTON_WIDTH, BUTTON_HEIGHT)
+play_button = pygame_gui.elements.UIButton(
+    play_button_rect,
+    "Play",
+    manager
+)
+settings_button_rect = pygame.Rect(WIDTH/2-BUTTON_WIDTH/2, 200, BUTTON_WIDTH, BUTTON_HEIGHT)
+settings_button = pygame_gui.elements.UIButton(
+    settings_button_rect,
+    "Settings",
+    manager
+)
+leave_button_rect = pygame.Rect(WIDTH/2-BUTTON_WIDTH/2, 250, BUTTON_WIDTH, BUTTON_HEIGHT)
+leave_button = pygame_gui.elements.UIButton(
+    leave_button_rect,
+    "Leave",
+    manager
+)
+
+menu_button_rect = pygame.Rect(10, 10, BUTTON_WIDTH, BUTTON_HEIGHT)
+menu_button = pygame_gui.elements.UIButton(
+    menu_button_rect,
+    "Menu",
+    manager
+)
+
+FPS_slider_rect = pygame.Rect(WIDTH/2-(BUTTON_WIDTH+30)/2, 100, BUTTON_WIDTH+30, BUTTON_HEIGHT)
+FPS_slider = pygame_gui.elements.UIHorizontalSlider(
+    FPS_slider_rect,
+    60,
+    (30, 120),
+    manager
+)
+FPS_label_rect = pygame.Rect(FPS_slider.get_relative_rect().x, FPS_slider.get_relative_rect().y + 35, BUTTON_WIDTH+30, BUTTON_HEIGHT)
+FPS_label = pygame_gui.elements.UILabel(
+    FPS_label_rect,
+    "FPS: 60",
+    manager
+)
+
+menu_buttons = [
+    play_button,
+    settings_button,
+    leave_button
+]
+playing_buttons = [
+    menu_button
+]
+settings_buttons = [
+    FPS_slider,
+    FPS_label
+]
 
 background = pygame.image.load("background.png")
 background_width = background.get_width()
@@ -36,7 +95,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Platform Game")
 
 class Enemy:
-    def __init__(self, x, y, platforms, bg):
+    def __init__(self, x, y, platforms):
         self.enemy_pos_x = x
         self.enemy_pos_y = y
         self.platforms = platforms
@@ -69,7 +128,7 @@ class Enemy:
         if self.enemy_pos_x + ENEMY_WIDTH > lowest_platform.x and self.enemy_pos_x < lowest_platform.right:
             if self.enemy_pos_y + ENEMY_HEIGHT >= lowest_platform.y:
                 self.falling = True
-                
+
     def check_platform_collision(self):
         self.falling = True
         for platform in self.platforms:
@@ -102,17 +161,17 @@ class Enemy:
             self.enemy_pos_x += self.direction
         else:
             self.direction *= -1
+            self.enemy_pos_x += self.direction
         
 
        
 
 class Player:
-    def __init__(self, x, y, platforms, bg):
+    def __init__(self, x, y, platforms):
         self.player_pos_x = x
         self.player_pos_y = y
         self.draw_player()
         self.platforms = platforms
-        self.bg = bg
         self.jumping = False
         self.velocity = 0
         self.falling = False
@@ -145,9 +204,6 @@ class Player:
                 if not self.jumping and not self.falling:
                     self.fall()
             
-
-            
-
     def fall(self):
         if not self.falling:
             if self.velocity > 0:
@@ -180,9 +236,6 @@ class Player:
         
         for platform in self.platforms:
             platform.x -= PLAYER_SPEED
-
-        for thing in self.bg:
-            thing.x -= PLAYER_SPEED
     
     def left(self):
         for platform in self.platforms:
@@ -193,15 +246,14 @@ class Player:
         for platform in self.platforms:
             platform.x += PLAYER_SPEED
 
-        for thing in self.bg:
-            thing.x += PLAYER_SPEED
-
     def player_movement(self):
         self.gravity()
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and keys[pygame.K_q]:
+            pass
+        elif keys[pygame.K_d]:
             self.right()
-        if keys[pygame.K_q]:
+        elif keys[pygame.K_q]:
             self.left()
         if keys[pygame.K_SPACE]:
             if not self.jumping and not self.falling:
@@ -212,20 +264,36 @@ class Player:
 
 class Game:
     def __init__(self):
-        self.platforms = []
-        self.bg = []
+        self.platforms = [] 
+        self.platforms = self.draw_platform()
+
+        #Everything[0] -> Platforms, Everything[1] -> Enemies, Everything[2] -> Other
+        self.everything = []
         self.scroll = 0
-        self.player = Player(STARTING_X, STARTING_Y, self.platforms, self.bg)
-        self.enemy = Enemy(ENEMY_STARTING_X, ENEMY_STARTING_Y, self.platforms, self.bg)
-        self.enemy2 = Enemy(ENEMY_STARTING_X, ENEMY_STARTING_Y + 80, self.platforms, self.bg)
+        self.just_lost = False
+        self.creating_player_enemies()
+
+    #Used to reset all the elements of the screen (if lost)
+    def creating_player_enemies(self):
+        self.player = Player(STARTING_X, STARTING_Y, self.platforms)
+        self.enemy = Enemy(ENEMY_STARTING_X, ENEMY_STARTING_Y, self.platforms)
+        self.enemy2 = Enemy(ENEMY_STARTING_X, ENEMY_STARTING_Y + 80, self.platforms)
+        self.enemy3 = Enemy(ENEMY_STARTING_X - 280, ENEMY_STARTING_Y - 50, self.platforms)
         self.enemies = []
 
         self.enemies.append(self.enemy)
-        self.enemies.append(self.enemy2)
+        #self.enemies.append(self.enemy2)
+        #self.enemies.append(self.enemy3)
+
+    def reseting_everything(self):
+        self.player = None
+        self.enemies = []
+        self.platforms = []
+
 
     def show_players(self):
-        self.player.player_movement()
-        self.player.draw_player()
+        self.player.player_movement()                                                                                                                                           #type: ignore
+        self.player.draw_player()                                                                                                                                               #type: ignore
 
         for enemy in self.enemies:
             enemy.draw_enemy()
@@ -233,18 +301,22 @@ class Game:
 
     def check_loss(self):
         for enemy in self.enemies:
-            if self.player.player_pos_y + self.player.player.h < enemy.enemy_pos_y:
+            if self.player.player_pos_y + self.player.player.h < enemy.enemy_pos_y:                                                                                             #type: ignore
                 continue
-            if self.player.player_pos_y > enemy.enemy_pos_y + enemy.enemy.h:
+            if self.player.player_pos_y > enemy.enemy_pos_y + enemy.enemy.h:                                                                                                    #type: ignore
                 continue
 
-            if self.player.player_pos_x + self.player.player.w >= enemy.enemy_pos_x and self.player.player_pos_x + self.player.player.w <= enemy.enemy_pos_x + enemy.enemy.w:
+            if self.player.player_pos_x + self.player.player.w >= enemy.enemy_pos_x and self.player.player_pos_x + self.player.player.w <= enemy.enemy_pos_x + enemy.enemy.w:   #type: ignore
                 self.lost()
-            elif self.player.player_pos_x <= enemy.enemy_pos_x + enemy.enemy.w and self.player.player_pos_x >= enemy.enemy_pos_x:
+            elif self.player.player_pos_x <= enemy.enemy_pos_x + enemy.enemy.w and self.player.player_pos_x >= enemy.enemy_pos_x:                                               #type: ignore
                self.lost()
 
     def lost(self):
-        print("loose")
+        self.reseting_everything()
+        self.creating_player_enemies()
+        self.scroll = 0
+
+        self.just_lost = True
 
     def draw_platform(self):
         base = pygame.Rect(0, 450, 10000, HEIGHT-450)
@@ -268,33 +340,29 @@ class Game:
         self.platforms.append(leftRect)
         #self.platforms.append(rightRect)
         self.platforms.append(centerRect2)
-        self.platforms.append(rightWall)
+        #self.platforms.append(rightWall)
         self.platforms.append(leftWall)
         return self.platforms
 
-    def draw_bg(self):
-        #for i in range(10):
-            #self.bg.append(pygame.Rect(WIDTH*i, 0, 5, 600))
-        
+    def draw_bg(self):  
         for i in range(-1, tiles):
             window.blit(background, (i*background_width + self.scroll, 0))
 
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and keys[pygame.K_q]:
+            pass
+        elif keys[pygame.K_d]:
             self.right()
-        if keys[pygame.K_q]:
+        elif keys[pygame.K_q]:
             self.left()
 
         if abs(self.scroll) > background_width:
             self.scroll = 0
 
-        return self.bg
-
     def right(self):
         for platform in self.platforms:
-            if self.player.player.y >= platform.y and self.player.player.y <= platform.y + platform.height:
-                if self.player.player.x + self.player.player.width < platform.x or self.player.player.x + self.player.player.width >= platform.x + platform.width:
+            if self.player.player.y >= platform.y and self.player.player.y <= platform.y + platform.height:                                                              #type: ignore
+                if self.player.player.x + self.player.player.width < platform.x or self.player.player.x + self.player.player.width >= platform.x + platform.width:       #type: ignore
                     pass
                 else:
                     return
@@ -306,8 +374,8 @@ class Game:
     
     def left(self):
         for platform in self.platforms:
-            if self.player.player.y >= platform.y and self.player.player.y <= platform.y + platform.height:
-                if self.player.player.x > platform.x + platform.width or self.player.player.x + self.player.player.width <= platform.x:
+            if self.player.player.y >= platform.y and self.player.player.y <= platform.y + platform.height:                                                              #type: ignore
+                if self.player.player.x > platform.x + platform.width or self.player.player.x + self.player.player.width <= platform.x:                                  #type: ignore
                     pass
                 else:
                     return
@@ -317,6 +385,42 @@ class Game:
         for enemy in self.enemies:
             enemy.enemy_pos_x += PLAYER_SPEED
 
+    def handle_slider(self):
+        FPS = round(FPS_slider.get_current_value(), 0)
+        FPS_label.set_text(f"FPS: {FPS_slider.get_current_value():.1f}")
+
+    def handle_buttons(self, event, playing):
+        if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == play_button:
+            for button in menu_buttons:
+                button.hide()
+            for button in playing_buttons:
+                button.show()
+            playing = True
+
+        elif event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == settings_button:
+            for button in menu_buttons:
+                button.hide()
+            for button in settings_buttons:
+                button.show()
+            menu_button.show()
+
+        elif event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == leave_button:
+            quit()
+
+        elif event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == menu_button:
+            self.lost()
+            for button in menu_buttons:
+                button.show()
+            for button in playing_buttons:
+                button.hide()
+            for button in settings_buttons:
+                button.hide()
+            playing = False
+
+        if not playing:
+            self.handle_slider()
+
+        return playing
 
 game = Game()
 
@@ -324,28 +428,46 @@ clock = pygame.time.Clock()
 
 def main():
     run = True
+    playing = False
     platforms = game.draw_platform()
-    bg = game.draw_bg()
+
+    for button in settings_buttons:
+        button.hide()
+    for button in playing_buttons:
+        button.hide()
+
     while run:
         clock.tick(FPS)
-
+        manager.update(clock.tick(FPS_UI) / 1000.0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+
+            manager.process_events(event)
+
+            playing = game.handle_buttons(event, playing)
+
+            if game.just_lost == True:
+                game.just_lost = False
+                platforms = game.draw_platform()
+            
         window.fill((0, 0, 0))
-        game.draw_bg()
 
-        game.show_players()
+        if playing:
+            game.draw_bg()
 
-        game.check_loss()
+            game.show_players()
 
-        for platform in platforms:
-            pygame.draw.rect(window, (123, 123, 123), platform)
+            game.check_loss()
 
-        for thing in bg:
-            pygame.draw.rect(window, (255, 255, 255), thing)
+            if game.just_lost == True:
+                game.just_lost = False
+                platforms = game.draw_platform()
 
+            for platform in platforms:
+                pygame.draw.rect(window, (123, 123, 123), platform)
+
+        manager.draw_ui(window)
         pygame.display.update()
 
 main()
